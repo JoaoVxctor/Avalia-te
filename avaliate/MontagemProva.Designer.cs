80790e8ba5f1921ef34fdb9f7982771f81008fab
Joao Victor <j7fernandes@gmail.com>
2018-11-25 02:25:58 -0200

    impressao quase feita

--- avaliate/MontagemProva.Designer.cs
+++ avaliate/MontagemProva.Designer.cs
@@ -30,31 +30,74 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MontagemProva));
             this.gradientPanel1 = new avaliate.gradientPanel();
+            this.header = new System.Windows.Forms.Button();
+            this.imprimir = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.listBox2 = new System.Windows.Forms.ListBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.imprimir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.printDocument = new System.Drawing.Printing.PrintDocument();
             this.printPreviewDialog = new System.Windows.Forms.PrintPreviewDialog();
             this.gradientPanel1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // gradientPanel1
             // 
             this.gradientPanel1.ColorBottom = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(59)))), ((int)(((byte)(209)))));
             this.gradientPanel1.ColorTop = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
+            this.gradientPanel1.Controls.Add(this.header);
             this.gradientPanel1.Controls.Add(this.imprimir);
             this.gradientPanel1.Controls.Add(this.label1);
             this.gradientPanel1.Controls.Add(this.groupBox2);
             this.gradientPanel1.Controls.Add(this.groupBox1);
+            this.gradientPanel1.Controls.Add(this.pictureBox1);
             this.gradientPanel1.Location = new System.Drawing.Point(0, 0);
             this.gradientPanel1.Name = "gradientPanel1";
-            this.gradientPanel1.Size = new System.Drawing.Size(880, 698);
+            this.gradientPanel1.Size = new System.Drawing.Size(880, 728);
             this.gradientPanel1.TabIndex = 0;
+            // 
+            // header
+            // 
+            this.header.AutoSize = true;
+            this.header.BackColor = System.Drawing.Color.Transparent;
+            this.header.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.header.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
+            this.header.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
+            this.header.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.header.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.header.ForeColor = System.Drawing.Color.White;
+            this.header.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.header.Location = new System.Drawing.Point(589, 87);
+            this.header.Name = "header";
+            this.header.Size = new System.Drawing.Size(206, 45);
+            this.header.TabIndex = 23;
+            this.header.Text = "Selecionar Cabeçalho";
+            this.header.UseVisualStyleBackColor = false;
+            this.header.Click += new System.EventHandler(this.header_Click);
+            // 
+            // imprimir
+            // 
+            this.imprimir.AutoSize = true;
+            this.imprimir.BackColor = System.Drawing.Color.Transparent;
+            this.imprimir.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.imprimir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
+            this.imprimir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
+            this.imprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.imprimir.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imprimir.ForeColor = System.Drawing.Color.White;
+            this.imprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.imprimir.Location = new System.Drawing.Point(647, 669);
+            this.imprimir.Name = "imprimir";
+            this.imprimir.Size = new System.Drawing.Size(195, 45);
+            this.imprimir.TabIndex = 22;
+            this.imprimir.Text = "Imprimir Prova";
+            this.imprimir.UseVisualStyleBackColor = false;
+            this.imprimir.Click += new System.EventHandler(this.imprimir_Click);
             // 
             // label1
             // 
@@ -62,7 +105,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label1.Location = new System.Drawing.Point(210, 92);
+            this.label1.Location = new System.Drawing.Point(210, 144);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(384, 29);
             this.label1.TabIndex = 2;
@@ -75,7 +118,7 @@
             this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox2.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.Color.White;
-            this.groupBox2.Location = new System.Drawing.Point(452, 124);
+            this.groupBox2.Location = new System.Drawing.Point(452, 176);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(408, 487);
             this.groupBox2.TabIndex = 1;
@@ -105,7 +148,7 @@
             this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.groupBox1.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(12, 124);
+            this.groupBox1.Location = new System.Drawing.Point(12, 176);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(401, 487);
             this.groupBox1.TabIndex = 0;
@@ -129,24 +172,16 @@
             this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
             this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
             // 
-            // imprimir
+            // pictureBox1
             // 
-            this.imprimir.AutoSize = true;
-            this.imprimir.BackColor = System.Drawing.Color.Transparent;
-            this.imprimir.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.imprimir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
-            this.imprimir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(67)))), ((int)(((byte)(184)))));
-            this.imprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.imprimir.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.imprimir.ForeColor = System.Drawing.Color.White;
-            this.imprimir.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.imprimir.Location = new System.Drawing.Point(647, 617);
-            this.imprimir.Name = "imprimir";
-            this.imprimir.Size = new System.Drawing.Size(195, 45);
-            this.imprimir.TabIndex = 22;
-            this.imprimir.Text = "Imprimir Prova";
-            this.imprimir.UseVisualStyleBackColor = false;
-            this.imprimir.Click += new System.EventHandler(this.imprimir_Click);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(877, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
             // 
             // printDocument
             // 
@@ -167,15 +202,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 690);
+            this.ClientSize = new System.Drawing.Size(872, 726);
             this.Controls.Add(this.gradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "MontagemProva";
             this.Text = "MontagemProva";
             this.gradientPanel1.ResumeLayout(false);
             this.gradientPanel1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -190,6 +227,8 @@
         private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Button imprimir;
         private System.Drawing.Printing.PrintDocument printDocument;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button header;
         private System.Windows.Forms.PrintPreviewDialog printPreviewDialog;
     }
 }
--- avaliate/MontagemProva.cs
+++ avaliate/MontagemProva.cs
@@ -106,9 +106,15 @@ namespace avaliate
         {
             int i=0;
             var fonte =  new Font("Times New Roman", 14);
-            foreach (string item in listBox2.Items){ 
+           
 
-            e.Graphics.DrawString(listBox2.Items[i].ToString(), fonte, Brushes.Black, new PointF(100, i*50));
+            //           Bitmap bm = new Bitmap(2480, 800);
+            //          pictureBox1.DrawToBitmap(bm, new Rectangle(100, 0, pictureBox1.Width, pictureBox1.Height
+            e.Graphics.DrawImage(pictureBox1.Image, 0, 0 );
+            //              bm.Dispose();
+            foreach (string item in listBox2.Items){
+
+            e.Graphics.DrawString(listBox2.Items[i].ToString(), fonte, Brushes.Black, new PointF(100, i*50 + pictureBox1.Image.Height));
             i++;
 
             }
@@ -119,5 +125,18 @@ namespace avaliate
             if (printPreviewDialog.ShowDialog() == DialogResult.OK)
                 printDocument.Print();
         }
+
+        private void header_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()
+            {
+                Filter =  "PNG|*png" , ValidateNames = true 
+            })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
     }
 }
